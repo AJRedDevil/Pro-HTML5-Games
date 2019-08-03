669dc3d98dcc79d06c613a184804eedd872464de
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-08-03 13:32:13 +0545

    feat(basics): Calling Drawing Loop with requestAnimationFrame()
    - browser vendors API specifically for handling animation
    - Advantages
    - 1. Optimize the animation code into a single reflow-and-repaint cycle, resulting in smoother animation
    - 2. Pause the animation when the tab is not visible, leading to less CPU and GPU usage
    - 3. Automatically cap the frame rate on machines that do not support higher frame
    rates, or increase the frame rate on machines that are capable of processing them
    - When we need to stop animating (when the game is paused, or has ended), we can use cancelAnimationFrame()
    
    Closes #5

--- chapter1/animation.js
+++ chapter1/animation.js
@@ -3,15 +3,19 @@ function animationLoop() {
   // And move them
 }
 
-function drawingLoop() {
-  // 0. Call the drawingLoop() method once after 20 milliseconds
-  const gameLoop = setTimeout(drawingLoop, 20);
-  // 1. Clear the canvas
-  // 2. Iterate through all items
-  // 3. And draw each item
+function drawingLoop(nowTime) {
+  // 1. Call the drawingLoop() method whenever the browser is ready to draw again
+  const gameLoop = requestAnimationFrame(drawingLoop);
+  // 2. Clear the canvas
+  // 3. Iterate through all items
+  // 4. Optionally use nowTime and the last nowTime to interpolate frames
+  // 5. And draw the items
 
   // Stop calling drawingLoop() and clear the gameLoop variable
   clearTimeout(gameLoop);
+
+  // Stop calling drawingLoop() and clear the gameLoop variable
+  cancelAnimationFrame(gameLoop);
 }
 
 function multipleCall() {
