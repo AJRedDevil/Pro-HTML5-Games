485e5e33d965a2527b6560e016c9eee81c36f1e4
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-08-03 13:21:38 +0545

    feat(basics): Typical Animation and Drawing Loop
    - The most common way to handle animation is by keeping a drawing function that gets called several times a second
    - Within this function, we iterate through all the game entities and draw them one by one
    
    #5

---
+++ chapter1/animation.js
@@ -0,0 +1,10 @@
+function animationLoop() {
+  // Iterate through all the items in the game
+  // And move them
+}
+
+function drawingLoop() {
+  // 1. Clear the canvas
+  // 2. Iterate through all items
+  // 3. And draw each item
+}
