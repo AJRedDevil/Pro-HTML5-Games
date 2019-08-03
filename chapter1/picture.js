17be28513180ef9519c061c37abb2a465617b839
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-08-03 13:10:19 +0545

    feat(basics): Drawing an Image Loaded in a Sprite Sheet
    - we load a single large sprite sheet where all the sprites are placed side by side
    - Drawing the sprite involves calculating the x and y offset of the sprite within the image and then drawing just the appropriate portion of the image
    - This method involves only a single HTTP request and only a single Image object per sprite sheet, along with a little more complexity in computing the sprite location within the image
    - In terms of utilization of network resources, this is significantly better
    - Fewer HTTP requests
    - Better compression
    - Faster load times
    
    Closes #4

--- chapter1/picture.js
+++ chapter1/picture.js
@@ -26,9 +26,28 @@ const imageLoader = () => ({
   },
 });
 
-function drawImageIndividually() {
+function drawImageIndividually(context) {
   // First: (Load individual images and store in a big array)
   // Three arguments: the element, and destination (x, y) coordinates
-  context image = imageArray[imageNumber];
+  const image = imageArray[imageNumber];
   context.drawImage(image, x, y);
+}
+
+function drawImageFromSprite(context) {
+  // First: (Load single sprite sheet image)
+  // Nine arguments: the element, source (x, y) coordinates,
+  // source width and height (for cropping),
+  // destination (x, y) coordinates, and
+  // destination width and height (resize)
+  context.drawImage(
+    this.spriteImage,
+    this.imageWidth * imageNumber,
+    0,
+    this.imageWidth,
+    this.imageHeight,
+    x,
+    y,
+    this.imageWidth,
+    this.imageHeight
+  );
 }
