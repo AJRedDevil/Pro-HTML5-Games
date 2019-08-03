c14eabbb683ac2a3190627fc692965665dd1a64e
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-08-03 13:02:49 +0545

    feat(basics): Simple Image Loader
    - load() method takes an image URL, and increases the totalImages counter each time it is called
    - It then dynamically creates an Image object and sets the object’s src property
    - Finally, it uses the object’s onload event handler to increment the loadedImages counter, and once the counter reaches totalImages, it sets the loaded variable back to true
    
    #4

--- chapter1/picture.js
+++ chapter1/picture.js
@@ -5,3 +5,23 @@ function loadImage() {
     alert('Image finished loading');
   };
 }
+
+const imageLoader = () => ({
+  loaded: true,
+  loadedImages: 0,
+  totalImages: 0,
+  load: url => {
+    this.totalImages++;
+    this.loaded = false;
+    let image = new Image();
+    image.src = url;
+    image.onload = () => {
+      imageLoader.loadedImages++;
+      if (imageLoader.loadedImages == imageLoader.totalImages) {
+        imageLoader.loaded = true;
+      }
+      image.onload = undefined;
+    };
+    return image;
+  },
+});
