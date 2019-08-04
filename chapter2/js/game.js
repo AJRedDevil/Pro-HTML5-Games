cba71c6f21c6af9a82d681cb278e816b93a51840
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-08-04 17:53:50 +0545

    feat(frootwars): A Basic game Object (js/game.js)
    - Define a JavaScript object called game with an init() function
    - This init() function first saves references to the game canvas and context so we can refer to them more easily using game.context and game.canvas
    - After that it hides all game layers and shows the game start screen using the hideScreens() and showScreen() methods
    - Next, we have three helper methods, hideScreens(), hideScreen(), and showScreen(), which modify the display CSS attribute to help us show or hide the menu screens that we created
    
    #7

---
+++ chapter2/js/game.js
@@ -0,0 +1,29 @@
+const game = {
+  // Start initializing object, preloading assets and display start screen
+  init: function() {
+    // Get handler for game canvas and context
+    game.canvas = document.getElementById('gamecanvas');
+    game.context = game.canvas.getContext('2d');
+
+    // Hide all game layers and display the start screen
+    game.hideScreens();
+    game.showScreen('gamestartscreen');
+  },
+
+  // Screen Show/hide
+  hideScreens: function() {
+    const screens = document.getElementsByClassName('gamelayer');
+    // Iterate through all the game layers and set their display to none
+    screens.forEach(screen => (screen.style.display = 'none'));
+  },
+
+  hideScreen: function(id) {
+    const screen = document.getElementById(id);
+    screen.style.display = 'none';
+  },
+
+  showScreen: function(id) {
+    const screen = document.getElementById(id);
+    screen.style.display = 'block';
+  },
+};
