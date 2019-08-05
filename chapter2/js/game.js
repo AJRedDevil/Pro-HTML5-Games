f41d35adf10924830895306e9832abe54bda9174
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-08-05 21:33:13 +0545

    feat(frootwars): The start() and animate() Functions Inside the game Object
    - The start() method
    - Initializes a few variables that we need in the game: offsetLeft and mode. offsetLeft will be used for panning the game view around the entire level, and mode will be used to store the current state of the game (intro, wait for firing, firing, fired)
    - Hides all other layers and displays the canvas layer and the score layer, which is a narrow bar on the top of the screen that contains the game score and a few game interface control elements
    - Sets the game animation interval to call the animate() function by using window. requestAnimationFrame
    - animate(), will do all the animation and drawing within our game. The method starts with calling a temporary placeholder handleGameLogic() method, which we will use to handle panning as well as the game control flow using game modes
    - parallax scrolling is a technique used to create an illusion of depth by moving background images slower than foreground images. this technique exploits the fact that objects at a distance always appear to move slower than objects that are close by.
    - We then draw the background and foreground images. For both the images, we first crop a canvas-sized portion of the image that is offset appropriately along the x-axis using the offsetLeft variable, and then draw it onto the canvas. One thing to note is that the background image and foreground image are moved
    at different speeds relative to the left offset: the background image is moved only one-fourth of the distance that the foreground image is moved. This difference in movement speed of the two layers will give us the illusion that the clouds are further away once we start panning around the level.
    - After the backgrounds, we draw the slingshot in the foreground, subtracting offsetLeft from its x-axis position so that the slingshot appears to stay in the same place while the game pans to the right.
    - Finally, we check if the game.ended flag has been set and, if not, use requestAnimationFrame to call animate() again. We can use the game.ended flag later to decide when to stop the animation loop.
    
    #11

--- chapter2/js/game.js
+++ chapter2/js/game.js
@@ -38,6 +38,108 @@ const game = {
     game.hideScreens();
     game.showScreen('levelselectscreen');
   },
+
+  // Store current game state - intro, wait-for-firing,
+  // fired, load - next - hero, success, failure
+  mode: 'intro',
+
+  // X & Y coordinates of the slingshot
+  slingshotX: 140,
+  slingshotY: 280,
+
+  // X & Y coordinate of point where band is attached to slingshot
+  slingshotBandX: 140 + 55,
+  slingshotBandY: 280 + 23,
+
+  // Flag to check if the game has ended
+  ended: false,
+
+  // The game score
+  score: 0,
+
+  // X axis offset for panning the screen from left to right
+  offsetLeft: 0,
+
+  start: function() {
+    game.hideScreens();
+    // Display the game canvas and score
+    game.showScreen('gamecanvas');
+    game.showScreen('scorescreen');
+
+    game.mode = 'intro';
+    game.currentHero = undefined;
+
+    game.offsetLeft = 0;
+    game.ended = false;
+
+    game.animationFrame = window.requestAnimationFrame(
+      game.animate,
+      game.canvas
+    );
+  },
+
+  handleGameLogic: function() {
+    // Temporary placeholder code. Keep panning the game towards the right
+    game.offsetLeft++;
+  },
+
+  animate: function() {
+    // Handle paning game states, and control flow
+    game.handleGameLogic();
+
+    // Draw the background with parallax scrolling
+    // First draw the background image, offset by a fraction
+    // of the offsetLeft distance(1 / 4)
+    // The bigger the fraction, the closer the background
+    // appears to be
+    game.context.drawImage(
+      game.currentLevel.backgroundImage,
+      game.offsetLeft / 4,
+      0,
+      game.canvas.width,
+      game.canvas.height,
+      0,
+      0,
+      game.canvas.width,
+      game.canvas.height
+    );
+    // Then draw the foreground image, offset by the entire
+    // offsetLeft distance
+    game.context.drawImage(
+      game.currentLevel.foregroundImage,
+      game.offsetLeft,
+      0,
+      game.canvas.width,
+      game.canvas.height,
+      0,
+      0,
+      game.canvas.width,
+      game.canvas.height
+    );
+
+    // Draw the base of the slingshot, offset by the
+    // entire offsetLeft distance
+    game.context.drawImage(
+      game.slingshotImage,
+      game.slingshotX - game.offsetLeft,
+      game.slingshotY
+    );
+
+    // Draw the front of the the slingshot, offset by the
+    // entire offsetLeft distance
+    game.context.drawImage(
+      game.slingshotFrontImage,
+      game.slingshotX - game.offsetLeft,
+      game.slingshotY
+    );
+
+    if (!game.ended) {
+      game.animationFrame = window.requestAnimationFrame(
+        game.animate,
+        game.canvas
+      );
+    }
+  },
 };
 
 // Initialize game once page has fully loaded
