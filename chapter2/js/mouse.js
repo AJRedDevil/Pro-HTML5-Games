d2034c0ee8a3f1fefcc79f11d10727d8b367980c
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-08-07 20:23:41 +0545

    feat(frootwars): Handling Mouse Events
    - This mouse object has an init() method that sets event handlers for when the mouse is moved, when a mouse button is pressed or released, and when the mouse leaves the canvas area.
    - mousemovehandler(): Uses the canvas’s getBoundingClientRect() method and the event object’s clientX and clientY properties to calculate the x and y coordinates of the mouse relative to the top-left corner of the canvas and stores them. It also checks whether the mouse button is pressed down while the mouse is being moved and, if so, sets the dragging variable to true.
    - mousedownhandler(): Sets the down variable to true.
    - mouseuphandler(): Sets the down and dragging variables to false. If the mouse leaves the canvas area, we call this same method.
    - All three methods additionally contain an extra line to prevent the default browser behavior for the mouse event.
    
    #12

---
+++ chapter2/js/mouse.js
@@ -0,0 +1,38 @@
+const mouse = {
+  x: 0,
+  y: 0,
+  down: false,
+  dragging: false,
+
+  init: function() {
+    const canvas = document.getElementById('gamecanvas');
+    canvas.addEventListener('mousemove', mouse.mousemovehandler, false);
+    canvas.addEventListener('mousedown', mouse.mousedownhandler, false);
+    canvas.addEventListener('mouseup', mouse.mouseuphandler, false);
+    canvas.addEventListener('mouseout', mouse.mouseouthandler, false);
+  },
+
+  mousemovehandler: function(ev) {
+    const offset = game.canvas.getBoundingClientRect();
+
+    mouse.x = ev.clientX - offset.left;
+    mouse.y = ev.clientY - offset.top;
+
+    if (mouse.down) {
+      mouse.dragging = true;
+    }
+    ev.preventDefault();
+  },
+
+  mousedownhandler: function(ev) {
+    mouse.down = true;
+    ev.preventDefault();
+  },
+
+  mouseuphandler: function() {
+    mouse.down = false;
+    mouse.dragging = false;
+
+    ev.preventDefault();
+  },
+};
